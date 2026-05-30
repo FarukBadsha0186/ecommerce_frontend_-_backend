3ae06b77dcec523bbc9e315b929964a90828a19f
FarukBadsha0186 <farukbadsha02@gmail.com>
2026-05-31 01:58:03 +0600

    fix port for render

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const app=require("./app");
-const PORT =5000;
+// const app=require("./app");
+// const PORT =5000;
+// app.listen(PORT, function(){
+//     console.log("App port @5000");
+// });
+const app = require("./app");
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, function(){
-    console.log("App port @5000");
+    console.log("App port @" + PORT);
 });
